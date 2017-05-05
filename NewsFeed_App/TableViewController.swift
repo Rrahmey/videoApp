32f2bde6588f76925959910f412790fe1b1607b7
Raquel Rahmey <rrahmey@aol.com>
2017-05-05 12:58:32 -0400

    initital commit

---
+++ NewsFeed_App/TableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TableViewController.swift
+//  NewsFeed_App
+//
+//  Created by Raquel Rahmey on 5/5/17.
+//  Copyright © 2017 Raquel Rahmey. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
